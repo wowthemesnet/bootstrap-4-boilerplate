aa9dbfde2382a559c3e1b0829a1297a271ff81a9
wowthemesnet <wowthemesnet@gmail.com>
2020-03-06 19:48:24 +0200

    partials, minification and more

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,90 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const gulpIf = require('gulp-if');
 const browserSync = require('browser-sync').create();
-const { src, dest } = require('gulp');
+const sass = require('gulp-sass');
+const htmlmin = require('gulp-htmlmin');
+const cssmin = require('gulp-cssmin');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const jsImport = require('gulp-js-import');
+const sourcemaps = require('gulp-sourcemaps');
+const htmlPartial = require('gulp-html-partial');
+const clean = require('gulp-clean');
+const isProd = process.env.NODE_ENV === 'prod';
 
-//compile scss into css
-function style() {
-    //1. scss location
-    return gulp.src('src/scss/**/*.scss') //gets all files ending with .scss in src/scss
-    //2. pass it through sass compiler
-    .pipe(sass().on('error',sass.logError))
-    //3. save the compiled css file
-    .pipe(gulp.dest('src/css'))
-    //4. stream  all browsers
-    .pipe(browserSync.stream());
+const htmlFile = [
+    'src/*.html'
+]
+
+function html() {
+    return gulp.src(htmlFile)
+        .pipe(htmlPartial({
+            basePath: 'src/partials/'
+        }))
+        .pipe(gulpIf(isProd, htmlmin({
+            collapseWhitespace: true
+        })))
+        .pipe(gulp.dest('docs'));
 }
 
+function css() {
+    return gulp.src('src/sass/style.scss')
+        .pipe(gulpIf(!isProd, sourcemaps.init()))
+        .pipe(sass({
+            includePaths: ['node_modules']
+        }).on('error', sass.logError))
+        .pipe(gulpIf(!isProd, sourcemaps.write()))
+        .pipe(gulpIf(isProd, cssmin()))
+        .pipe(gulp.dest('docs/css/'));
+}
 
-function watch() {
+function js() {
+    return gulp.src('src/js/*.js')
+        .pipe(jsImport({
+            hideConsole: true
+        }))
+        .pipe(concat('all.js'))
+        .pipe(gulpIf(isProd, uglify()))
+        .pipe(gulp.dest('docs/js'));
+}
+
+function img() {
+    return gulp.src('src/img/*')
+        .pipe(gulpIf(isProd, imagemin()))
+        .pipe(gulp.dest('docs/img/'));
+}
+
+function serve() {
     browserSync.init({
-        server: {
-            baseDir: "./src",
-            index: "/index.html"
-        }
+        open: true,
+        server: './docs'
     });
-    gulp.watch('src/scss/**/*.scss', style);    
-    gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
-    gulp.watch('./*.html').on('change',browserSync.reload);
 }
 
-function build() {
-    return src('./src/**')
-      .pipe(dest('./docs/'));
-  }
+function browserSyncReload(done) {
+    browserSync.reload();
+    done();
+}
 
-exports.style = style;
-exports.watch = watch;
-exports.build = build;
+
+function watchFiles() {
+    gulp.watch('src/**/*.html', gulp.series(html, browserSyncReload));
+    gulp.watch('src/**/*.scss', gulp.series(css, browserSyncReload));
+    gulp.watch('src/**/*.js', gulp.series(js, browserSyncReload));
+    gulp.watch('src/img/**/*.*', gulp.series(img));
+
+    return;
+}
+
+function del() {
+    return gulp.src('docs/*', {read: false})
+        .pipe(clean());
+}
+
+exports.css = css;
+exports.html = html;
+exports.js = js;
+exports.del = del;
+exports.serve = gulp.parallel(html, css, js, img, watchFiles, serve);
+exports.default = gulp.series(del, html, css, js, img);
